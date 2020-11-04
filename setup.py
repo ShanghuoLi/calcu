9119f23fe3277ced95ee8780dd61b807fe56e5f9
Shanghuo Li <shanghuo.li@gmail.com>
2020-11-04 16:06:30 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
   name = 'calcu',
-  packages = ['calcu'],
   version = '0.1.0',
   description = 'To compute the physical parameters of molecular outflow and total column density of molecule',
   author = ['Shanghuo Li'],
   author_email = 'shanghuo.li@gmail.com',
   url = 'https://github.com/ShanghuoLi/calcu', # use the URL to the github repo  
   packages=find_packages(),
-  keywords = ['astrophysics', 'calcu', 'outflow', 'column density'], # arbitrary keywords
-  classifiers = [],
-  install_requires=[
+  classifiers = [
       'numpy',
       'matplotlib',
       'astropy',
